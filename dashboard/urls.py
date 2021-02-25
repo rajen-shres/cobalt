c839d6997172e1dfa3dc9d0d40fc1b99879f0f34
Tanmay Dutta <tanmay.datta86@gmail.com>
2021-02-25 12:15:33 +1100

    First draft for help

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #    path("", views.home, name="home"),
     path("", views.dashboard, name="dashboard"),
     path("logged-out", views.logged_out, name="logged_out"),
+    path("help", views.help, name="help"),
     path("scroll1", views.scroll1, name="scroll1"),
     path("scroll2", views.scroll2, name="scroll2"),
 ]
