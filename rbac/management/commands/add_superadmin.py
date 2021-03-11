53f031918da86faf52fc48a2975602b9e8a051f6
Tanmay Dutta <tanmay.datta86@gmail.com>
2021-03-11 12:48:26 +1100

    added Julian as super admin

---
+++ rbac/management/commands/add_superadmin.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from rbac.management.commands.rbac_core import (
+    create_RBAC_admin_group,
+    create_RBAC_admin_tree,
+)
+from rbac.core import rbac_add_user_to_admin_group, rbac_add_role_to_admin_group
+from organisations.models import Organisation
+from accounts.models import User
+
+""" Create static within RBAC for Organisations
+
+    Creates a super admin group
+    tree and rights to manage tree from top level.
+
+
+"""
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("Running add_superadmin")
+        userList = [User.objects.filter(username=name).first() for name in ["tanmay", "518891"]]
+        super_group = create_RBAC_admin_group(self, "admin", "", "Super admin group")
+        create_RBAC_admin_tree(self, super_group, "rbac")
+        for user in userList:
+            rbac_add_user_to_admin_group(super_group, user)
