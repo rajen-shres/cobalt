168f8b069b20de92effd7706a308fc27abbaacd0
Mark Guthrie <m@rkguthrie.com>
2021-06-05 17:29:31 +1000

    Django upgrade

--- cobalt/settings.py
+++ cobalt/settings.py
@@ -191,7 +191,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    #    "django.middleware.common.BrokenLinkEmailsMiddleware",
 ]
 
 ROOT_URLCONF = "cobalt.urls"
--- cobalt/urls.py
+++ cobalt/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
 from dashboard.views import logged_out, home
+from django.conf import settings
+from django.urls import include, path
+
 
 # Overwrite admin panel defaults
 admin.site.site_header = f"{settings.GLOBAL_ORG}Tech Administration"
