567c8b0cd38a8a48d3571451b51870a1d417e788
Mark Guthrie <m@rkguthrie.com>
2021-08-07 13:11:31 +1000

    testing

--- utils/cgit/new_window.sh
+++ utils/cgit/new_window.sh
@@ -4,4 +4,19 @@ window_count="$(osascript utils/cgit/new_window_count.scpt)"
 
 echo $window_count
 
-osascript utils/cgit/new_window_open.scpt
+osascript << EOF
+tell application "System Events"
+	tell process "Terminal"
+		tell menu item "Test Layout" of menu "Open Window Group" of menu item "Open Window Group" of menu "Window" of menu bar item "Window" of menu bar 1
+			click
+		end tell
+	end tell
+end tell
+    tell application "Terminal"
+      activate
+      do script with command "echo Window A" in window $((window_count+1))
+      do script with command "echo Window A3" in window 3
+      -- do script with command "stripe listen --forward-to 127.0.0.1:8088/payments/stripe-webhook" in window $((window_count+2))
+      do script with command "echo Window C" in window $((window_count+3))
+    end tell
+EOF
