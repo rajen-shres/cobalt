797933763d7348a2bbfdb2f2c6ef6ce5928be7b4
Mark Guthrie <mark.guthrie@abf.com.au>
2021-08-17 20:28:15 +1000

    email

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,10 +1,15 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring
 from django.urls import path
 from . import views
+from django_ses.views import SESEventWebhookView
+from django.views.decorators.csrf import csrf_exempt
 
 app_name = "notifications"  # pylint: disable=invalid-name
 
 urlpatterns = [
+    path(
+        "ses/event-webhook/", SESEventWebhookView.as_view(), name="handle-event-webhook"
+    ),
     path("", views.homepage, name="homepage"),
     path("passthrough/<int:id>/", views.passthrough, name="passthrough"),
     path("deleteall", views.deleteall, name="deleteall"),
