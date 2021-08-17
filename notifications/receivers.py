1c403331daed53a7681e63b359d1abad80cf974b
Mark Guthrie <mark.guthrie@abf.com.au>
2021-08-17 23:11:48 +1000

    email

--- notifications/receivers.py
+++ notifications/receivers.py
@@ -4,6 +4,6 @@ from django.dispatch import receiver
 
 @receiver(send_received)
 def send_handler(sender, mail_obj, send_obj, raw_message, *args, **kwargs):
-    print("Aardvark")
-    print(sender)
-    print(mail_obj)
+    print("Aardvark 4", flush=True)
+    print(sender, flush=True)
+    print(mail_obj, flush=True)
