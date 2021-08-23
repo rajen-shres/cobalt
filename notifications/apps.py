81449a2b8ed61b0f57848bb987b84ff003cc25ba
Mark Guthrie <mark.guthrie@abf.com.au>
2021-08-23 20:06:01 +1000

    debug ses signals

--- notifications/apps.py
+++ notifications/apps.py
@@ -55,7 +55,7 @@ class NotificationsConfig(AppConfig):
 
             return None
 
-        def send_handler(sender, mail_obj, send_obj, raw_message, *args):
+        def send_handler(sender, mail_obj, send_obj, raw_message, *args, **kwargs):
             """Handle SES incoming info"""
 
             logger.info("inside send handler")
