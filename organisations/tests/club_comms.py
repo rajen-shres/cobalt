a3bc0c06a28b3447cbdb5ae3b5a05a90f73cdc7a
Mark Guthrie <m@rkguthrie.com>
2021-09-16 21:54:25 +1000

    comms

--- organisations/tests/club_comms.py
+++ organisations/tests/club_comms.py
@@ -37,13 +37,13 @@ class ClubComms:
     def a1_comms_tags(self):
         """Do things with tags"""
 
-        # Login as Colin
+        # Login as Gary
         login_and_go_to_club_menu(
             manager=self.manager,
             org_id=SUNSHINE_ID,
-            user=self.manager.colin,
-            test_description="Login as Colin and go to club menu",
-            test_name="Login as Colin and go to club menu",
+            user=self.manager.gary,
+            test_description="Login as Gary and go to club menu",
+            test_name="Login as Gary and go to club menu",
             reverse_result=False,
         )
 
@@ -52,7 +52,7 @@ class ClubComms:
             manager=self.manager,
             tab="comms",
             title_id="t_tab_heading_comms",
-            test_name=f"Go to comms tab as Colin for {club_names[SUNSHINE_ID]}",
+            test_name=f"Go to comms tab as Gary for {club_names[SUNSHINE_ID]}",
             test_description="Starting from the dashboard of Club Menu we click on the Communications tab "
             "and confirm that we get there.",
         )
@@ -66,9 +66,9 @@ class ClubComms:
 
         self.manager.save_results(
             status=True,
-            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Colin (admin).",
-            test_name=f"Colin adds tag 'Grumpy Buggers' for {club_names[SUNSHINE_ID]}",
-            test_description=f"Colin goes to the comms tab for {club_names[SUNSHINE_ID]}. "
+            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Gary (admin).",
+            test_name=f"Gary adds tag 'Grumpy Buggers' for {club_names[SUNSHINE_ID]}",
+            test_description=f"Gary goes to the comms tab for {club_names[SUNSHINE_ID]}. "
             f"He then clicks on tags and adds a new one.",
         )
 
@@ -79,9 +79,9 @@ class ClubComms:
 
         self.manager.save_results(
             status=True,
-            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Colin (admin).",
-            test_name=f"Colin adds tag 'Nice People' for {club_names[SUNSHINE_ID]}",
-            test_description=f"Colin goes to the comms tab for {club_names[SUNSHINE_ID]}. "
+            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Gary (admin).",
+            test_name=f"Gary adds tag 'Nice People' for {club_names[SUNSHINE_ID]}",
+            test_description=f"Gary goes to the comms tab for {club_names[SUNSHINE_ID]}. "
             f"He then clicks on tags and adds a new one.",
         )
 
@@ -92,8 +92,41 @@ class ClubComms:
 
         self.manager.save_results(
             status=True,
-            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Colin (admin).",
-            test_name=f"Colin adds tag 'Geniuses' for {club_names[SUNSHINE_ID]}",
-            test_description=f"Colin goes to the comms tab for {club_names[SUNSHINE_ID]}. "
+            output=f"Added a new tag for {club_names[SUNSHINE_ID]} as Gary (admin).",
+            test_name=f"Gary adds tag 'Geniuses' for {club_names[SUNSHINE_ID]}",
+            test_description=f"Gary goes to the comms tab for {club_names[SUNSHINE_ID]}. "
             f"He then clicks on tags and adds a new one.",
+        )
+
+        # Delete a tag - there are no nice people in bridge
+        self.manager.selenium_wait_for_text("Geniuses", "id_panel_comms")
+        self.manager.selenium_wait_for_clickable("t_delete_tag_2").click()
+        self.manager.selenium_wait_for_text("Geniuses", "id_panel_comms")
+
+        self.manager.save_results(
+            status=True,
+            output=f"Deleted a tag for {club_names[SUNSHINE_ID]} as Gary (admin).",
+            test_name=f"Gary delets tag 'Nice People' for {club_names[SUNSHINE_ID]}",
+            test_description=f"Gary is on the comms tab for {club_names[SUNSHINE_ID]}. "
+            f"He deletes a tag.",
+        )
+
+        # Login as Gary
+        login_and_go_to_club_menu(
+            manager=self.manager,
+            org_id=SUNSHINE_ID,
+            user=self.manager.gary,
+            test_description="Login as Gary and go to club menu",
+            test_name="Login as Gary and go to club menu",
+            reverse_result=False,
+        )
+
+        # Go to Comms tab
+        club_menu_go_to_tab(
+            manager=self.manager,
+            tab="comms",
+            title_id="t_tab_heading_comms",
+            test_name=f"Go to comms tab as Gary for {club_names[SUNSHINE_ID]}",
+            test_description="Starting from the dashboard of Club Menu we click on the Communications tab "
+            "and confirm that we get there.",
         )
