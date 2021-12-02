857c2274956bba3c4b47c00b93eae480bceb6db6
Mark Guthrie <m@rkguthrie.com>
2021-12-02 20:26:51 +1100

    tests

--- accounts/tests/unit/unit_thing.py
+++ accounts/tests/unit/unit_thing.py
@@ -1,3 +1,10 @@
-def test_my_thing():
+import pytest
 
-    assert 3 == 3
+from accounts.models import User
+
+
+@pytest.mark.django_db
+def test_my_thing():
+    user = User.objects.filter(system_number=620246).first()
+
+    assert user.first_name == "Mark"
--- conftest.py
+++ conftest.py
@@ -1,5 +1,33 @@
-import pytest
-
-
-def pytest_html_report_title(report):
-    report.title = "your title!"
+# import pytest
+#
+# from cobalt.settings import set_value
+#
+# pytestmark = pytest.mark.django_db
+#
+#
+# @pytest.fixture()
+# def django_db_setup():
+#
+#     RDS_DB_NAME = set_value("RDS_DB_NAME")
+#     RDS_USERNAME = set_value("RDS_USERNAME")
+#     RDS_PASSWORD = set_value("RDS_PASSWORD")
+#     RDS_HOSTNAME = set_value("RDS_HOSTNAME")
+#     RDS_PORT = set_value("RDS_PORT")
+#
+#     settings.DATABASES["default"] = {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": RDS_DB_NAME,
+#         "USER": RDS_USERNAME,
+#         "PASSWORD": RDS_PASSWORD,
+#         "HOST": RDS_HOSTNAME,
+#         "PORT": RDS_PORT,
+#     }
+#
+#
+# @pytest.fixture(autouse=True)
+# def auto_fixture():
+#     """This is automatically loaded every time we run pytest"""
+#
+#
+# def pytest_html_report_title(report):
+#     report.title = "your title!"
