189aaca3424677966b5989bb5a5a5f4751f89f7f
Mark Guthrie <m@rkguthrie.com>
2022-03-30 09:23:09 +1100

    club admin - just after migration issues

--- events/migrations/0105_alter_congress_congress_type.py
+++ events/migrations/0105_alter_congress_congress_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("events", "0104_auto_20220318_1346"),
+        ("events", "0104_auto_20220323_2047"),
     ]
 
     operations = [
