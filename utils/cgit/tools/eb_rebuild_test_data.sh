987ea45a2dd549869ecf0f2a7abc83108b37e604
Mark Guthrie <m@rkguthrie.com>
2022-04-04 15:56:21 +1000

    test db scripts

--- utils/cgit/tools/eb_rebuild_test_data.sh
+++ utils/cgit/tools/eb_rebuild_test_data.sh
@@ -3,4 +3,12 @@
 
 . /var/app/current/utils/cgit/tools/eb_env_setup.sh
 
+# Double check we are on an environment we expect to be on
+if [ "$COBALT_HOSTNAME" = "test.myabf.com.au" ] || [ "$COBALT_HOSTNAME" = "uat.myabf.com.au" ]
+then
+
 utils/aws/rebuild_test_database_subcommands.sh
+
+else
+  echo "$COBALT_HOSTNAME not on list of expected hosts. Aborting."
+fi
