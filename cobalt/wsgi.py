048732beb534586e3b5cc821f3b986e2ba72e601
Mark Guthrie <m@rkguthrie.com>
2022-04-27 10:19:09 +1000

    new relic

--- cobalt/wsgi.py
+++ cobalt/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
+import newrelic.agent
 
-import os
+newrelic.agent.initialize("/etc/newrelic-infra.yml")
 
-from django.core.wsgi import get_wsgi_application
+import os  # noqa: 402
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cobalt.settings')
+from django.core.wsgi import get_wsgi_application  # noqa: 402
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cobalt.settings")
 
 application = get_wsgi_application()
