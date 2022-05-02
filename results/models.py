389201f4157c12241a06f274bdf3953fb9dbb5d4
Mark Guthrie <m@rkguthrie.com>
2022-05-03 09:24:03 +1000

    results

--- results/models.py
+++ results/models.py
@@ -1,3 +1,68 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from accounts.models import User
+from organisations.models import Organisation
+
+
+def _results_file_directory_path(instance, filename):
+    """We want to save results files in a club folder"""
+
+    return f"results/club_{instance.organisation.id}/{timezone.now().year}-{timezone.now().month}-{timezone.now().day}/{filename}"
+
+
+class ResultsFile(models.Model):
+    """Initially this supports clubs uploading files in USEBIO format. This may need to be extended for other
+    formats and we will also need to decide if we use files at all when/if we put scoring into Cobalt"""
+
+    class ResultsStatus(models.TextChoices):
+        PUBLISHED = "PU"
+        PENDING = "PE"
+
+    results_file = models.FileField(upload_to=_results_file_directory_path)
+    uploaded_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+    )
+    organisation = models.ForeignKey(
+        Organisation,
+        on_delete=models.PROTECT,
+    )
+    status = models.CharField(
+        max_length=2, choices=ResultsStatus.choices, default=ResultsStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.results_file.path}"
+
+    def filename(self):
+        return os.path.basename(self.results_file.name)
+
+
+class PlayerSummaryResult(models.Model):
+    """Short summary of a players results, for high level views"""
+
+    player_system_number = models.PositiveIntegerField()
+    """ system number of player who played in this event """
+    result_date = models.DateTimeField()
+    """ date event took place """
+    position = models.PositiveIntegerField(blank=True, null=True)
+    """ optional - where the player finished """
+    percentage = models.FloatField(blank=True, null=True)
+    """ optional what percentage they got """
+    result_string = models.CharField(max_length=200, null=True, blank=True)
+    """ de-normalised string to show as summary """
+    event_name = models.CharField(max_length=200, null=True, blank=True)
+    """ name of event """
+    results_file = models.ForeignKey(
+        ResultsFile,
+        on_delete=models.PROTECT,
+    )
+    """ linked results file - may need to make this optional later depending on
+        whether we generate files when we do scoring ourselves  """
+
+    def __str__(self):
+        return f"{self.player_system_number} - {self.event_name}"
--- results/views/core.py
+++ results/views/core.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 import json
 import xmltodict
+from django.utils.datetime_safe import datetime
+
+from cobalt.settings import MEDIA_ROOT
+from results.models import ResultsFile, PlayerSummaryResult
+from django.contrib.humanize.templatetags.humanize import ordinal
 
 
 @login_required
@@ -9,12 +14,42 @@ def home(request):
     return render(request, "results/home.html")
 
 
-def parse_usebio_file(filename):
-    """read a USEBIO format XML file and turn into a dictionary"""
+def parse_usebio_file(results_file):
+    """read a USEBIO format XML file that has already been uploaded and turn into a dictionary"""
 
-    with open(filename, "rb") as file:
+    file_name = f"{MEDIA_ROOT}/{results_file.results_file.name}"
+
+    with open(file_name, "rb") as file:
         xml = file.read()
 
     xml = xmltodict.parse(xml)
 
     return xml["USEBIO"]["EVENT"]
+
+
+def create_player_records_from_usebio_format(results_file: ResultsFile, xml: dict):
+    """take in a xml usebio structure and generate the PlayerSummaryResult records"""
+
+    #    results_file = get_object_or_404(ResultsFile, pk=results_file_id)
+
+    event_name = xml["TITLE"]
+    event_date_str = xml["DATE"]
+    event_date = datetime.strptime(event_date_str, "%d/%m/%Y")
+
+    for detail in xml["PARTICIPANTS"]["PAIR"]:
+        percentage = detail["PERCENTAGE"]
+        position = detail["PLACE"]
+        place = ordinal(position)
+        for player in detail["PLAYER"]:
+            player_system_number = player["NATIONAL_ID_NUMBER"]
+
+            if player_system_number:
+                PlayerSummaryResult(
+                    player_system_number=player_system_number,
+                    results_file=results_file,
+                    result_date=event_date,
+                    position=position,
+                    percentage=percentage,
+                    result_string=f"{place} in {event_name} at {results_file.organisation}",
+                    event_name=event_name,
+                ).save()
