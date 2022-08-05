89740dd9c2afd42a3b721d76172783a053e479e7
Mark Guthrie <m@rkguthrie.com>
2022-08-06 09:17:50 +1000

    Results - use MPs not score for order (unfinished)

--- results/views/results_views.py
+++ results/views/results_views.py
@@ -14,7 +14,7 @@ from results.views.usebio import parse_usebio_file
 def _get_player_names_by_id(usebio):
     """Helper to get the player names, numbers and directions"""
 
-    player_dict = {"system_numbers": {}, "names": {}, "direction": {}}
+    player_dict = {"system_numbers": {}, "names": {}}
     for item in usebio["PARTICIPANTS"]["PAIR"]:
 
         # Names
@@ -37,10 +37,22 @@ def _get_player_names_by_id(usebio):
             player_2_system_number,
         ]
 
-        # Direction
-        player_dict["direction"][this_pair_id] = item.get("DIRECTION")
-
     return player_dict
+
+
+def _get_pair_direction_for_board(usebio, board_number, pair):
+    """Get which direction a pair was sitting for a particular board"""
+
+    for board_item in usebio["EVENT"]["BOARD"]:
+        if int(board_item["BOARD_NUMBER"]) == board_number:
+            for score in board_item["TRAVELLER_LINE"]:
+                if score["NS_PAIR_NUMBER"] == pair:
+                    return True
+                if score["EW_PAIR_NUMBER"] == pair:
+                    return False
+
+    # Default to NS if we don't know
+    return True
 
 
 def _format_pair_name(player_1, player_2):
@@ -370,7 +382,7 @@ def usebio_mp_pairs_details_view(request, results_file_id, pair_id):
 @login_required()
 def usebio_mp_pairs_board_view(request, results_file_id, board_number, pair_id):
     """Show the traveller for a board. If pair_id is provided then we show it from the
-    perspective of that pair"""
+    perspective of that pair. Pair id will be 0 if not provided"""
 
     results_file = get_object_or_404(ResultsFile, pk=results_file_id)
     usebio = parse_usebio_file(results_file)
@@ -378,8 +390,9 @@ def usebio_mp_pairs_board_view(request, results_file_id, board_number, pair_id):
     # First get all the players names and numbers
     player_dict = _get_player_names_by_id(usebio["EVENT"])
 
-    # If we got a pair_number, then see which way they were sitting on this board, default view to NS
-    ns_flag = True if pair_id == 0 else player_dict["direction"][pair_id] == "NS"
+    # get direction of pair on this board
+    ns_flag = _get_pair_direction_for_board(usebio, board_number, pair_id)
+
     board_data = get_traveller_info(
         usebio, board_number, player_dict, ns_flag, pair_id, request
     )
@@ -399,30 +412,29 @@ def usebio_mp_pairs_board_view(request, results_file_id, board_number, pair_id):
                 }
 
             double_dummy = double_dummy_from_usebio(board["HAND"])
+            break
 
     if not double_dummy:
         return HttpResponse(f"Board {board_number} not found for this result")
+
+    if ns_flag:
+        board_data = sorted(board_data, key=lambda d: -d["ns_match_points"])
+    else:
+        board_data = sorted(board_data, key=lambda d: -d["ew_match_points"])
 
     dealer, vulnerability = dealer_and_vulnerability_for_board(board_number)
     par_score, par_string = par_score_and_contract(double_dummy, vulnerability, dealer)
 
     # Add par score into the data
-    row = {
-        "score": par_score,
-        "par_score": par_score,
-        "par_string": par_string,
-    }
-    board_data.append(row)
+    # row = {
+    #     "score": par_score,
+    #     "par_score": par_score,
+    #     "par_string": par_string,
+    # }
+    # board_data.append(row)
 
-    # Sort up or down depending on who is viewing it
-    try:
-        if ns_flag:
-            board_data = sorted(board_data, key=lambda d: -d["score"])
-        else:
-            board_data = sorted(board_data, key=lambda d: d["score"])
-    except TypeError:
-        # score may be averaged or adjusted, not a number
-        pass
+    # We need to work out where in the match point table this would fit
+    # for item in board_data:
 
     # Add High card points and losing trick count
     high_card_points, losing_trick_count = calculate_hcp_and_ltc(hand)
@@ -457,80 +469,91 @@ def usebio_mp_pairs_board_view(request, results_file_id, board_number, pair_id):
     )
 
 
-def get_traveller_info(usebio, board_number, player_dict, ns_flag, pair_id, request):
-    """extract traveller information about a board from a usebio record"""
+def _get_traveller_info_process_board(
+    board, player_dict, pair_id, board_number, ns_flag, request
+):
+    """sub of get_traveller_info to process the record"""
 
     board_data = []
+
+    for traveller_line in board.get("TRAVELLER_LINE"):
+        ns_pair_number = traveller_line.get("NS_PAIR_NUMBER")
+        ns_pair = player_dict["names"].get(ns_pair_number)
+        ew_pair_number = traveller_line.get("EW_PAIR_NUMBER")
+        ew_pair = player_dict["names"].get(ew_pair_number)
+        contract = traveller_line.get("CONTRACT")
+        played_by = traveller_line.get("PLAYED_BY")
+        lead = traveller_line.get("LEAD")
+        tricks = traveller_line.get("TRICKS")
+        score = traveller_line.get("SCORE")
+        ns_match_points = traveller_line.get("NS_MATCH_POINTS")
+        ew_match_points = traveller_line.get("EW_MATCH_POINTS")
+        try:
+            score = int(score)
+        except ValueError:
+            pass
+        # TODO: Test and make more robust
+
+        # Calculate percentage and score
+        if type(score) is str:
+            # Score is adjusted
+            percentage = int(score[1:3])
+            score = 0
+        else:
+            ew_match_points = float(traveller_line.get("EW_MATCH_POINTS"))
+            ns_match_points = float(traveller_line.get("NS_MATCH_POINTS"))
+            # Normal numeric score
+            percentage = _percentage_from_match_points(
+                ns_match_points, ew_match_points, ns_flag
+            )
+
+        # indicator = _set_indicator_based_on_percentage(percentage)
+        indicator = _set_icon_based_on_percentage(percentage)
+
+        # highlight row of interest
+        if pair_id in [ns_pair_number, ew_pair_number]:
+            if request.user.system_number in player_dict["system_numbers"][pair_id]:
+                # This user
+                tr_highlight = "bg-warning"
+            else:
+                # Another user
+                tr_highlight = "bg-info"
+        else:
+            tr_highlight = ""
+
+        row = {
+            "board_number": board_number,
+            "contract": contract,
+            "played_by": played_by,
+            "lead": lead,
+            "tricks": tricks,
+            "score": score,
+            "percentage": percentage,
+            "ns_pair_number": ns_pair_number,
+            "ew_pair_number": ew_pair_number,
+            "ns_pair": ns_pair,
+            "ew_pair": ew_pair,
+            "tr_highlight": tr_highlight,
+            "indicator": indicator,
+            "ns_match_points": ns_match_points,
+            "ew_match_points": ew_match_points,
+        }
+
+        board_data.append(row)
+
+    return board_data
+
+
+def get_traveller_info(usebio, board_number, player_dict, ns_flag, pair_id, request):
+    """extract traveller information about a board from a usebio record"""
 
     for board in usebio["EVENT"]["BOARD"]:
         this_board_number = int(board.get("BOARD_NUMBER"))
         if this_board_number == board_number:
             # Found our board - go through the traveller lines
-            for traveller_line in board.get("TRAVELLER_LINE"):
-                ns_pair_number = traveller_line.get("NS_PAIR_NUMBER")
-                ns_pair = player_dict["names"].get(ns_pair_number)
-                ew_pair_number = traveller_line.get("EW_PAIR_NUMBER")
-                ew_pair = player_dict["names"].get(ew_pair_number)
-                contract = traveller_line.get("CONTRACT")
-                played_by = traveller_line.get("PLAYED_BY")
-                lead = traveller_line.get("LEAD")
-                tricks = traveller_line.get("TRICKS")
-                score = traveller_line.get("SCORE")
-                try:
-                    score = int(score)
-                except ValueError:
-                    pass
-                # TODO: Test and make more robust
-
-                # Calculate percentage and score
-                if type(score) is str:
-                    # Score is adjusted
-                    percentage = int(score[1:3])
-                    score = 0
-                else:
-                    ew_match_points = float(traveller_line.get("EW_MATCH_POINTS"))
-                    ns_match_points = float(traveller_line.get("NS_MATCH_POINTS"))
-                    # Normal numeric score
-                    percentage = _percentage_from_match_points(
-                        ns_match_points, ew_match_points, ns_flag
-                    )
-
-                # indicator = _set_indicator_based_on_percentage(percentage)
-                indicator = _set_icon_based_on_percentage(percentage)
-
-                # highlight row of interest
-                if pair_id in [ns_pair_number, ew_pair_number]:
-                    if (
-                        request.user.system_number
-                        in player_dict["system_numbers"][pair_id]
-                    ):
-                        # This user
-                        tr_highlight = "bg-warning"
-                    else:
-                        # Another user
-                        tr_highlight = "bg-info"
-                else:
-                    tr_highlight = ""
-
-                row = {
-                    "board_number": board_number,
-                    "contract": contract,
-                    "played_by": played_by,
-                    "lead": lead,
-                    "tricks": tricks,
-                    "score": score,
-                    "percentage": percentage,
-                    "ns_pair_number": ns_pair_number,
-                    "ew_pair_number": ew_pair_number,
-                    "ns_pair": ns_pair,
-                    "ew_pair": ew_pair,
-                    "tr_highlight": tr_highlight,
-                    "indicator": indicator,
-                }
-
-                board_data.append(row)
-
-    return board_data
+            return _get_traveller_info_process_board(
+                board, player_dict, pair_id, board_number, ns_flag, request
+            )
 
 
 def calculate_hcp_and_ltc(hand):
