018143cfa8f2385264960bb2bbdd2eb2201966a2
Mark Guthrie <m@rkguthrie.com>
2022-09-09 12:45:16 +1000

    gunicorn

--- cobalt/wsgi.py
+++ cobalt/wsgi.py
@@ -1,17 +1,16 @@
 """
 WSGI config for cobalt project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 import os
-import newrelic.agent
 
-NEWRELIC_INI = "/cobalt-media/admin/__confdata_/newrelic.ini"
-if os.path.exists(NEWRELIC_INI):
-    newrelic.agent.initialize("/cobalt-media/admin/__confdata_/newrelic.ini")
+# import newrelic.agent
+#
+# NEWRELIC_INI = "/cobalt-media/admin/__confdata_/newrelic.ini"
+# if os.path.exists(NEWRELIC_INI):
+#     newrelic.agent.initialize("/cobalt-media/admin/__confdata_/newrelic.ini")
 
 from django.core.wsgi import get_wsgi_application  # noqa: 402
 
