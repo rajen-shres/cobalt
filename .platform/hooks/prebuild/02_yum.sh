8c6599e3de218db365250c44c9385f9309222b6f
Mark Guthrie <mark.guthrie@abf.com.au>
2022-10-19 11:39:13 +1100

    aws cli

--- .platform/hooks/prebuild/02_yum.sh
+++ .platform/hooks/prebuild/02_yum.sh
@@ -3,3 +3,12 @@
 yum -y install amazon-efs-utils
 yum -y install postgresql.x86_64
 yum -y install git
+
+# Install AWS CLI if not present
+if [ ! -f "/bin/aws" ]; then
+  cd /tmp
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip -o awscliv2.zip
+  ./aws/install
+fi
+
