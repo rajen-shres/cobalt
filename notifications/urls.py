750cf471d7ed4b23b5928e7725a23a80739a40d7
Mark Guthrie <m@rkguthrie.com>
2023-03-02 10:27:01 +1100

    investigate email problems

--- notifications/urls.py
+++ notifications/urls.py
@@ -12,16 +12,16 @@ import notifications.views.redirect
 app_name = "notifications"  # pylint: disable=invalid-name
 
 urlpatterns = [
-    # path(
-    #     "ses/event-webhook/",
-    #     SESEventWebhookView.as_view(),
-    #     name="handle-event-webhook",
-    # ),
     path(
         "ses/event-webhook/",
-        notifications.views.aws.debug,
+        SESEventWebhookView.as_view(),
         name="handle-event-webhook",
     ),
+    # path(
+    #     "ses/event-webhook/",
+    #     notifications.views.aws.debug,
+    #     name="handle-event-webhook",
+    # ),
     path("", notifications.views.user.homepage, name="homepage"),
     path(
         "passthrough/<int:id>/",
