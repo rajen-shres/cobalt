9b67d615bfe1c82110d378daf1abbafa451f6c98
Drew Kennelly <drewkennelly@gmail.com>
2025-02-27 14:11:48 +1300

    install rust

---
+++ .platform/hooks/prebuild/00_install_rust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing Rust..." >> /var/log/cobalt.log
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Make Rust available to subsequent scripts
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> /root/.bashrc
+echo 'source $HOME/.cargo/env' >> /root/.bashrc
+
+# Also make it available to the webapp user
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> /home/webapp/.bashrc
+echo 'source $HOME/.cargo/env' >> /home/webapp/.bashrc
+
+echo "Rust installation complete" >> /var/log/cobalt.log 
